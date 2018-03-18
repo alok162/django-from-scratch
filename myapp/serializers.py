75b984c4ec2318a6d7efee46ccbae4f38d7a06f2
Alok <alok@Aloks-MacBook-Pro.local>
2018-03-18 11:54:56 +0530

    removed create() and update() using modelserializer which is internal inbuilt recognise create and update

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -2,11 +2,15 @@ from rest_framework import serializers
 from myapp.models import Users
 
 
-class UsersSerializer(serializers.Serializer):
-	name = serializers.CharField(max_length=200)
+class UsersSerializer(serializers.ModelSerializer):
+		class Meta:
+			model = Users
+			fields = ['name']
 
-	def create(self, validated_data):
-		return Users.objects.create(**validated_data)
+	
+	# def create(self, validated_data):
+	# 	return Users.objects.create(**validated_data)
+
 	# def update(self, instance, validated_data):
 	# 	instance.name = validated_data.get('name', instance.name)
 	# 	instance.save()
